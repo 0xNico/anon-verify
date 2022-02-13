96ff10451cb9ca3a1cb09595d68b76beb02febd0
zkRemda <zkremda@riseup.net>
2022-02-12 19:23:46 -0500

    feat: add wallet context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
-
+import dynamic from 'next/dynamic';
+import {ReactNode} from 'react'
+const WalletContextProvider = dynamic<{ children: ReactNode }>(
+  () =>
+    import("../Components/WalletContext/WalletContext").then(
+      ({ WalletContext }) => WalletContext
+    ),
+  {
+    ssr: false,
+  }
+);
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WalletContextProvider>
+      <Component {...pageProps} />
+    </WalletContextProvider>
+  );
 }
